ffc5a4d4d90b7f94f7ea10e1c55cfb0d269564b4
gungmasmay <gungmas6@gmail.com>
2026-07-01 03:44:24 +0800

    component statecard

--- resources/views/components/statecard.blade.php
+++ resources/views/components/statecard.blade.php
@@ -1,31 +1,31 @@
-<!-- props([
+@props([
     'title',
     'value',
     'label' => '',
     'bg' => 'bg-white',
-    'border' => 'border-l-primary',
+    'border' => 'border-green-200',
     'iconBg' => 'bg-gray-100',
-]) -->
+])
 
-<div {{ $attributes->class(['flex-1 w-full h-fit', $bg, $border, 'border-l-8 rounded-4xl p-7 font-sans shadow-xl font-bold'])
+<div {{ $attributes->class(['flex-1 justify-between flex flex-col w-full grow', $bg, $border, 'border-t-8 rounded-xl p-7 font-monsterrat shadow-xl font-bold'])
     }}>
     <div class="flex justify-between gap-10 items-center mb-4.5">
         <h2 class="text-base font-bold text-subtext">
-            <!-- {{ $title }} -->
+            {{ $title }}
         </h2>
         <div class="{{ $iconBg }} p-2 rounded-lg">
-            <!-- {{ $slot }} -->
+            {{ $slot }}
         </div>
     </div>
 
     <div class="flex gap-2">
         <span class="text-4xl leading-none font-bold text-judul">
-            <!-- {{ $value }} -->
+            {{ $value }}
         </span>
 
         @if($label)
             <span class="text-base font-semibold text-dark-grey flex items-end">
-                <!-- {{ $label }} -->
+                {{ $label }}
             </span>
         @endif
     </div>
