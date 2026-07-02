ec156a5268be678513809acee8fd460a7cf2dc3a
C <134027205+404x-eng@users.noreply.github.com>
2026-07-02 12:42:17 +0800

    Revert "Merge remote-tracking branch 'origin/staging'"
    This reverts commit 50a80e961ca353402c5c3770d176b7a74ca24383, reversing
    changes made to a135d37adb771da4e5c82e89f9c9f05f5270d000.

--- resources/views/components/icons/back-arrow-kembali.blade.php
+++
@@ -1,3 +0,0 @@
-<svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1.24463 7.22227H15.9113M7.84463 0.622265L1.24463 7.22227L7.84463 13.8223" stroke="white" stroke-width="1.76"/>
-</svg>
