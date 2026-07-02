e01479a50876d96bfc2edf669527c61ea1afa4d0
gungmasmay <gungmas6@gmail.com>
2026-07-02 12:36:18 +0800

    buat detail

---
+++ app/Livewire/Admin/VerifikasiSkp/Detail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\VerifikasiSkp;
+
+use Livewire\Component;
+
+class Detail extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.verifikasi-skp.detail');
+    }
+}
